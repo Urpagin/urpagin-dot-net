d094d84d1b020131d9491db2ca31fb188a6ddbd6
Urpagin <urpagin@example.com>
2024-04-15 02:28:59 +0200

    Added docker and sh script to update container

---
+++ update-container.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+container_name="my-container-name"
+container_port=8000
+
+# Check if the container exists
+if sudo docker inspect $container_name &> /dev/null; then
+    echo "Removing container: $container_name"
+    sudo docker rm -f $container_name
+else
+    echo "Container $container_name does not exist, skipping removal."
+fi
+
+# Build the new image
+if sudo docker build -t $container_name:latest .; then
+    echo "Build succeeded, running container..."
+    # Run the new container instance
+    if sudo docker run --name $container_name -d -p $container_port:8000 --restart=unless-stopped $container_name:latest; then
+        echo "Container is up and running on port $container_port"
+    else
+        echo "Failed to run container"
+        exit 1
+    fi
+else
+    echo "Build failed"
+    exit 1
+fi
