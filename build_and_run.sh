54baeacdeb339357ac610cc8e4db7bc467f61288
Urpagin <urpagin@proton.me>
2025-01-13 18:10:11 +0100

    delete container in script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Remove any existing container named "urpagin-dot-net"
+sudo docker rm -f urpagin-dot-net || true
+
+# Build the new image
 sudo docker build . -t urpagin-dot-net
 
+# Run the new container
 sudo docker run --name urpagin-dot-net -d -p 35037:3000 urpagin-dot-net
