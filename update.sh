cb840e048ed0f903e5914a4b5ebd9987c4a98942
Urpagin <urpagin@proton.me>
2025-07-29 23:41:16 +0200

    tweak update.sk

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 git pull
 
 # Rebuild & recreate your service in one step
-sudo docker compose up -d --build urpagin-dot-net
+sudo docker compose up -d --build urpagin-dot-net --force-recreate
